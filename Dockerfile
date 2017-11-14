b58af5e749f67b3d222b35d7bd5ab8ecf8df7d32
Rick Guyer <ricoguyer@gmail.com>
2017-11-14 12:07:59 -0600

    Allow more config settings to be passed

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ MAINTAINER Rick Guyer <ricoguyer@gmail.com>
 COPY nginx.conf /etc/nginx/nginx.conf
 ADD entrypoint.sh /usr/sbin/
 
+ENV PHP_ADDR php:9000
 ENV APP_ENV docker
 ENV APP_ROOT /var/www/site/webroot
+ENV APP_INDEX index.php
 
 ENTRYPOINT ["entrypoint.sh"]
 CMD ["nginx"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-sed -i "s/%php-ip%/$PHP_PORT_9000_TCP_ADDR/" /etc/nginx/nginx.conf
+sed -i "s/%php-ip%/$PHP_ADDR/" /etc/nginx/nginx.conf
 sed -i "s;%app-root%;$APP_ROOT;" /etc/nginx/nginx.conf
 sed -i "s/%app-env%/$APP_ENV/" /etc/nginx/nginx.conf
+sed -i "s/%app-index%/$APP_INDEX/" /etc/nginx/nginx.conf
 exec $@
